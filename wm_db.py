b8708ca5acd4688c13e3c3cf47b29e22ef637192
luca <luca.carabetta@gmail.com>
2016-10-16 17:22:19 +0200

    Inseriti i controlli sul Json

--- wm_db.py
+++ wm_db.py
@@ -5,12 +5,69 @@ _password	= "washomatic"
 _host		= "52.210.163.71"
 _database	= "washomatic"
 
+
 def insert_weight(box_id, weight):
+	current_weight = get_current_weight(box_id)							# l'ultimo current_weight
+	set_box_last_weight(box_id, current_weight)							# viene spostato su last_weight nel db
 	cnx = mysql.connector.connect(user=_user, password=_password, host=_host, database=_database)
 	cursor = cnx.cursor()
-	# Devo prima salvare la lettura precedente nell'altro campo della tabella
 	query = """UPDATE box SET box_current_weight = %s WHERE box_id = %s"""
-	cursor.execute(query, (weight, box_id))						# Inserimento dei miei parametri
+	cursor.execute(query, (weight, box_id))
 	cnx.commit()
 	cursor.close()
 	cnx.close()
+
+
+# restituisce l'ultimo peso registrato relativamente a un box dato
+def get_current_weight(box_id):
+	cnx = mysql.connector.connect(user=_user, password=_password, host=_host, database=_database)
+	cursor = cnx.cursor(buffered=True)
+	query = """SELECT box_current_weight FROM box WHERE box_id = %s"""
+	cursor.execute(query, (box_id,))
+	cnx.commit()
+	for (box_current_weight,) in cursor:
+		cursor.close()
+		cnx.close()
+		return box_current_weight
+
+
+# Imposta un valore per la vecchia lettura del peso
+def set_box_last_weight(box_id, weight):
+	cnx = mysql.connector.connect(user=_user, password=_password, host=_host, database=_database)
+	cursor = cnx.cursor()
+	query = """UPDATE box SET box_last_weight = %s WHERE box_id = %s"""
+	cursor.execute(query, (weight, box_id))
+	cnx.commit()
+	cursor.close()
+	cnx.close()
+
+
+# Restituisce un booleano sulla presenza o meno del box nel db
+def boxExists(box_id):
+	cnx = mysql.connector.connect(user=_user, password=_password, host=_host, database=_database)
+	cursor = cnx.cursor(buffered=True)
+	query = """SELECT * FROM box WHERE box_id = %s"""
+	cursor.execute(query, (box_id,))
+	cnx.commit()
+	result = True
+	if cursor.rowcount == 0:
+		result = False
+	cursor.close()
+	cnx.close()
+	return result
+
+
+# Restituisce un booleano sulla presenza o meno del box nel db
+def controllerExists(controller_id):
+	cnx = mysql.connector.connect(user=_user, password=_password, host=_host, database=_database)
+	cursor = cnx.cursor(buffered=True)
+	query = """SELECT * FROM controller WHERE controller_id = %s"""
+	cursor.execute(query, (controller_id,))
+	cnx.commit()
+	result = True
+	if cursor.rowcount == 0:
+		result = False
+	cursor.close()
+	cnx.close()
+	return result
+
--- wm_parser.py
+++ wm_parser.py
@@ -3,18 +3,71 @@ import json
 import wm_db
 CMDs = {'WEIGHT_CHANGED':'0'}						# dictionary con la lista dei comandi
 
+def validController(msg):
+	try:
+		controller_id = msg['controller_id']
+	except KeyError:
+		print "invalid key"
+		return -1	
+	if not controller_id.isnumeric():		
+		print "non numeric controller id"
+		return False
+	if not wm_db.controllerExists(controller_id):
+		print "this controller does not exist"
+		return False
+	return True
+
+
+def validBox(msg):
+	try:
+		box_id = msg['box_id']
+	except KeyError:
+		print "invalid key"
+		return -1	
+	if not box_id.isnumeric():
+		print "non numeric box id"
+		return False
+	if not wm_db.boxExists(box_id):
+		print "this box does not exist"
+		return False
+	return True
+
+
+def validWeight(msg):
+	try:
+		weight = msg['w']
+	except KeyError:
+		print "invalid key"
+		return False	
+	if not weight.isnumeric():
+		print "non numeric value for weight"
+		return False
+	weight = float(weight)
+	if weight < 0:
+		print "invalid weight"
+		return False
+	return True
+
 def parse(msg):
-	try:								# per json non corretti si solleva un'eccezione
+	try:
 		arr = json.loads(msg)
 	except ValueError:
 		print "Invalid Json"
 
 	print "Valid Json"
+	
+	try:
+		cmd = arr['CMD']
+	except KeyError:
+		print "invalid key"
+		return -1
 
-	if arr['CMD'] == CMDs['WEIGHT_CHANGED']:			# "switch" sui diversi comandi che arrivano
-		print "Received command: WEIGHT_CHANGED - new weight: "+ arr['w'] +"kg"
-		#Qui check sui dati e scrittura su DB
+	if cmd == CMDs['WEIGHT_CHANGED']:
+		if not validController(arr) or not validBox(arr) or not validWeight(arr):
+			return -1
+		print "Received command: WEIGHT_CHANGED - new weight: "+ arr['w'] +"kg for box "+ arr['box_id']
 		wm_db.insert_weight(arr['box_id'], arr['w'])
 	else:
 		print "Command not found"
+
 
